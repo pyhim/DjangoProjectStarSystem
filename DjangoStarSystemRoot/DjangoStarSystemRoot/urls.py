dbf459509bdf5164046e4ffcf2093b8f4ad2c6f5
pyhim <galitskiy.dima8@gmail.com>
2021-07-26 13:56:08 +0300

    Connected Django-Smuggler

--- DjangoStarSystemRoot/DjangoStarSystemRoot/urls.py
+++ DjangoStarSystemRoot/DjangoStarSystemRoot/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("StarSystem.urls")),
+    path('', include('StarSystem.urls')),
+    path('admin/', include('smuggler.urls')),
     path('admin/', admin.site.urls),
 ]
